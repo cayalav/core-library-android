19435257a9820e9e0f682b20d003d7eb328cbd7d
Carlos Ayala <carlos.ayala@investors.com>
2022-08-18 22:49:56 -0500

    .

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+@Library('bpd-pipeline-library')_
+import org.bpd.AndroidHelper
+def androidUtils = new AndroidHelper(this)
+//def ARTIFACTS_BUILD = " assembleDev"
+ARTIFACT_URL_LISTS  = []
+ARTIFACT_TO_APPCENTER_LIST  = []
+
+pipeline {
+    
+    agent { label 'BuildServer' };
+    
+    environment{
+        ENV_HELLO="Hello World!" 
+    }
+
+    stages{
+        stage('hello-world'){
+            steps{
+              script {
+                  echo "${ENV_HELLO}"
+              }
+			
+            }
+        }
+    }
+}
