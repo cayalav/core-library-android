ac23b3cd377730034f1a9953c627bd865d5f708c
Carlos Ayala <carlos.ayala@investors.com>
2022-08-22 16:06:18 -0500

    .

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,6 +10,7 @@ pipeline {
             steps{
                 script {
 		            echo "${ENV_HELLO}"
+                    ls -la
                     //gradle --version
                 }
             }
